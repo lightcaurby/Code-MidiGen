96b8bda26b2fc5672d3504555296047c59880a64
lightcaurby <lightcaurby@gmail.com>
2020-11-20 19:50:52 +0200

    Renaming to MidiGen.

--- MidiGen/MidiGen.cpp
+++ MidiGen/MidiGen.cpp
@@ -1,5 +1,4 @@
-// sysexTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
-// sysexTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// MidiGen.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include "pch.h"
 #include <stdio.h>
